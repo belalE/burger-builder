4ed397f9bf1c2a49074764d77c10761ea6e8faea
belalE <belalelsiesy@gmail.com>
2022-05-23 23:37:22 -0500

    adjusting App and structure

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import Aux from '../../../hoc/Aux'
+import React, { Component } from 'react'
+
+import Aux from '../../../hoc/Aux/Aux'
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
     .map(igKey => {
         return (<li key={igKey}>
-        <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+        <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
         </li>);
     })
     return (
@@ -16,12 +19,14 @@ const orderSummary = (props) => {
         <ul>
             {ingredientSummary}
         </ul>
-        <p><strong>Total Price: {props.price}</strong></p>
+        <p><strong>Total Price: {this.props.price}</strong></p>
         <p>Continue to Checkout?</p>
-        <Button clicked={props.purchaseCanceled} btnType="Danger">CANCEL</Button>
-        <Button clicked={props.purchaseContinued} btnType="Success">CONTINUE</Button>
+        <Button clicked={this.props.purchaseCanceled} btnType="Danger">CANCEL</Button>
+        <Button clicked={this.props.purchaseContinued} btnType="Success">CONTINUE</Button>
     </Aux>
     )
+    }
+    
 }
 
-export default orderSummary
+export default OrderSummary
